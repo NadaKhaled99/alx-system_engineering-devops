efdd4407048d50ab93186bd86160ba34023d4d9f
Nada <nadafarrag619@gmail.com>
2024-01-31 03:23:27 +0200

    file

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-#the regular expression that will match the above cases
-#Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
 puts ARGV[0].scan(/hbt+n/).join
