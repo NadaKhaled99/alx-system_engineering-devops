8e8ae18d75f22e0afb3921d42ff5b58429a95324
Nada <nadafarrag619@gmail.com>
2024-04-22 20:54:53 +0200

    file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,22 +6,19 @@ Holder Place
 if __name__ == "__main__":
     import requests
     from sys import argv
-
     if len(argv) < 2:
         exit()
-    
     todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={A}&completed=true"
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
         .format(argv[1]))
-    
     name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={A}"
+        "https://jsonplaceholder.typicode.com/users?id={}"
         .format(argv[1]))
     name = name.json()
     name = name[0]["name"]
     
     todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={A}".format(argv[1]))
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
     todo = todo.json()
     todo = len(todo)
     todos = todos.json()
